e194bf6bc68d673c900bc23cdd2890debea597e5
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2023-10-31 00:10:14 +0000

    adding border

--- src/component/ObjectToolbar.tsx
+++ src/component/ObjectToolbar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import LineWeightIcon from "@mui/icons-material/LineWeight";
 import "./ObjectToolbar.css";
 
 export const ObjectToolbar = (props: {
@@ -8,6 +9,9 @@ export const ObjectToolbar = (props: {
     <div>
       <button onClick={() => props.setSection("color")}>
         <div className="color-button"></div>
+      </button>
+      <button onClick={() => {}}>
+        <LineWeightIcon />
       </button>
     </div>
   );
